25925c956c9647e2535fd622c73bda1344696246
dmitry <dkalinin240@gmail.com>
2025-04-08 14:20:10 +0300

    --

--- src/Console/Server.java
+++ src/Console/Server.java
@@ -29,7 +29,7 @@ public class Server {
     }
 
     public void run() {
-        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+        try (ServerSocket serverSocket = new ServerSocket(9813)) {
             logger.info("Сервер запущен");
 
             while (true) {
